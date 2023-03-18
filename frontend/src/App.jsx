75df701ea473cbb1307759e2c609673acff2d546
temelehtone <91484193+temelehtone@users.noreply.github.com>
2023-03-18 16:17:53 +0200

    fix vite config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,10 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  console.log(import.meta.env.VITE_WEATHER_API_KEY);
   return (
     <div className="App">
-      
+       
     </div>
   )
 }
